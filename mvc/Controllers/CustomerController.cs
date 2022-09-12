3928f8820ac9a7c65be71f1489456fbc9d6d7139
Bilal Karakan <bilalkaraka36@gmail.com>
2022-09-12 22:58:35 +0300

    Sending data using model

---
+++ mvc/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc.Models;
+
+namespace Mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            Customer customer = new()
+            {
+                FirstName = "Bilal",
+                LastName = "Karakan",
+                Age = 23
+            };
+            return View(customer);
+        }
+    }
+}
